6fbc63659004a5e2d0828e9677c4d09475766499
Jordy <jordynldroid@gmail.com>
2017-11-17 00:36:39 +0100

    Debug: If the debugger catches bugs then the module.lua file will be deleted.

--- src/python/debug.py
+++ src/python/debug.py
@@ -2,14 +2,15 @@ import subprocess
 import os
 
 def run(path):
-    filePath = "{}\{}".format(path, "debug.lua")
     try:
         subprocess.check_output(['lua', 'debug.lua'], cwd=path)
     except subprocess.CalledProcessError:
         buildStatus = ("• [!] Failed. Possibly contains errors and bugs.")
+        os.remove("{}\{}".format(path, "module.lua"))
     else:
+
         buildStatus = "• [+] Passed. No errors were detected at first run."
     finally:
-        os.remove(filePath)
+        os.remove("{}\{}".format(path, "debug.lua"))
         print(buildStatus)
         
