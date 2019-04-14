f1383552c13ce06351f45512fafa0ad7e40b97b0
Jordy19 <jordynldroid@gmail.com>
2019-04-14 22:19:21 +0200

    Build: The strings.lua file is located at "include.strings" not "src.lua.strings".

--- build.py
+++ build.py
@@ -23,7 +23,7 @@ class Builder():
         }
         self.loadOrder = [
             "src.lua.lib",
-            "src.lua.strings",
+            "include.strings",
             "src.lua.database",
             "src.lua.misc",
             "src.lua.init",
