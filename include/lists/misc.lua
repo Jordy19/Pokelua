bd84d1f4642edc5d7381452ad90d504faea59276
Jordy19 <jordynldroid@gmail.com>
2019-04-15 23:08:52 +0200

    Items: Merged Misc with Items list.

--- include/lists/misc.lua
+++ include/lists/misc.lua
@@ -1,10 +1,191 @@
--- db.objects.Manaphyegg={db={left={normal="1534c06f211",shiny="",10,10},right={normal="1534c06f211",shiny="",10,10},gender=1,evolution={1,{1,1}},types={"?",{1,1}},weakness={1,1,1},height="?",weight=1,skill="?",region="misc"}}
--- db.objects.Missingno={db={left={normal="1534c02391e",shiny="",0,10},right={normal="15cd43c988d",shiny="",0,10},gender=1,evolution={1,{1,1}},types={"?",{1,1}},weakness={1,1,1},height="?",weight=1,skill="?",region="misc"}}
--- db.objects.Missingno2={db={left={normal="1534c00c8d3",shiny="",10,15},right={normal="15cd43d2fed",shiny="",10,15},gender=1,evolution={1,{1,1}},types={"?",{1,1}},weakness={1,1,1},height="?",weight=1,skill="?",region="misc"}}
--- db.objects.Missingno3={db={left={normal="1534c06d1e7",shiny="",10,15},right={normal="15cd43e317c",shiny="",10,15},gender=1,evolution={1,{1,1}},types={"?",{1,1}},weakness={1,1,1},height="?",weight=1,skill="?",region="misc"}}
--- db.objects.Missingno4={db={left={normal="1534c05ba7d",shiny="",10,15},right={normal="15cd43e824d",shiny="",10,15},gender=1,evolution={1,{1,1}},types={"?",{1,1}},weakness={1,1,1},height="?",weight=1,skill="?",region="misc"}}
--- -- Trainers work in progress
--- db.objects.Trainer={db={left={normal="15cd44857e2",shiny="",10,-60},right={normal="15cd448403b",shiny="",10,-60},gender=1,evolution={1,{1,1}},types={"?",{1,1}},weakness={1,1,1},height="?",weight=1,skill="?",region="trainer"}}
--- db.objects.Trainer2={db={left={normal="15cd448706a",shiny="",10,-60},right={normal="15cd4488813",shiny="",10,-60},gender=1,evolution={1,{1,1}},types={"?",{1,1}},weakness={1,1,1},height="?",weight=1,skill="?",region="trainer"}}
--- db.objects.Trainer3={db={left={normal="15d610edcf3",shiny="",0,-14},right={normal="15d610ef4fa",shiny="",0,-14},gender=1,evolution={1,{1,1}},types={"?",{1,1}},weakness={1,1,1},height="?",weight=1,skill="?",region="trainer"}}
--- db.objects.Trainer4={db={left={normal="15cf8bb88eb",shiny="",0,-12},right={normal="15cf9e60de2",shiny="",0,-12},gender=1,evolution={1,{1,1}},types={"?",{1,1}},weakness={1,1,1},height="?",weight=1,skill="?",region="trainer"}}
+db.objects.Pokeball={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"1534bff7873", "1534bff7873"},
+        shiny={"", ""},
+        left={44, 50},
+        right={nil, nil},
+    }
+}
+
+db.objects.Masterball={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"157bfac671b", "157bfac671b"},
+        shiny={"", ""},
+        left={44, 50},
+        right={nil, nil},
+    }
+}
+
+db.objects.Egg={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"1534bfe985e", "1534bfe985e"},
+        shiny={"", ""},
+        left={10, 10},
+        right={nil, nil},
+    }
+}
+
+db.objects.Manaphyegg={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"1534c06f211", "1534c06f211"},
+        shiny={"", ""},
+        left={10, 10},
+        right={nil, nil},
+    }
+}
+
+db.objects.Missingno={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"1534c02391e", "15cd43c988d"},
+        shiny={"", ""},
+        left={0, 10},
+        right={nil, nil},
+    }
+}
+
+db.objects.Missingno2={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"1534c00c8d3", "15cd43d2fed"},
+        shiny={"", ""},
+        left={10, 15},
+        right={nil, nil},
+    }
+}
+
+db.objects.Missingno3={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"1534c06d1e7", "15cd43e317c"},
+        shiny={"", ""},
+        left={10, 15},
+        right={nil, nil},
+    }
+}
+
+db.objects.Missingno4={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"1534c05ba7d", "15cd43e824d"},
+        shiny={"", ""},
+        left={10, 15},
+        right={nil, nil},
+    }
+}
+
+db.objects.Trainer={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"15cd44857e2", "15cd448403b"},
+        shiny={"", ""},
+        left={10, -60},
+        right={nil, nil},
+    }
+}
+
+db.objects.Trainer2={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"15cd448706a", "15cd4488813"},
+        shiny={"", ""},
+        left={10, -60},
+        right={nil, nil},
+    }
+}
+
+db.objects.Trainer3={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"15d610edcf3", "15d610ef4fa"},
+        shiny={"", ""},
+        left={0, -14},
+        right={nil, nil},
+    }
+}
+
+db.objects.Trainer4={
+    id=000,
+    species="Item/Misc",
+    types={1},
+    region="Item",
+    gender=0,
+    height=0,
+    weight=0,
+    images={
+        normal={"15cf8bb88eb", "15cf9e60de2"},
+        shiny={"", ""},
+        left={0, -12},
+        right={nil, nil},
+    }
+}
