5831495e755abe80eaf5fbfe1b0f61500f6198a8
Jordy19 <Jordynldroid@gmail.com>
2019-05-20 15:28:01 +0200

    Commands: We can include name and args as arguments for command functions.

--- src/module/commands/dev.lua
+++ src/module/commands/dev.lua
@@ -12,7 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License. ]]
 
-function killFunction()
+function killFunction(name, args)
     --[[Help command.]]
     print("DIE!")
 end
--- src/module/commands/public.lua
+++ src/module/commands/public.lua
@@ -12,7 +12,7 @@
 -- See the License for the specific language governing permissions and
 -- limitations under the License. ]]
 
-function helpFunction()
+function helpFunction(name, args)
     --[[Help command.]]
     print("HELP!")
 end
