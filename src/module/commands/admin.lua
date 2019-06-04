2f0f28b47035c901e848718f5bf359d48d374504
Jordy19 <Jordynldroid@gmail.com>
2019-06-04 20:52:32 +0200

    Added new admin commands: !cheese, !shaman, !revive, !kill and !new-game.

--- src/module/commands/admin.lua
+++ src/module/commands/admin.lua
@@ -11,3 +11,59 @@
 -- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 -- See the License for the specific language governing permissions and
 -- limitations under the License. ]]
+
+cmd.cheese = {
+    'admin',
+    function (name, args)
+        local command = tfm.exec.giveCheese
+        if args[1] then
+            multiUserCommand(command, args)
+        else
+            command(name)
+        end
+    end
+}
+
+cmd.shaman = {
+    'admin',
+    function (name, args)
+        local command = tfm.exec.shaman
+        if args[1] then
+            multiUserCommand(command, args)
+        else
+            command(name)
+        end
+    end
+}
+
+cmd.kill = {
+    'admin',
+    function (name, args)
+        local command = tfm.exec.killPlayer
+        if args[1] then
+            multiUserCommand(command, args)
+        else
+            command(name)
+        end
+    end
+}
+
+cmd.revive = {
+    'admin',
+    function (name, args)
+        local command = tfm.exec.respawnPlayer()
+        if args[1] then
+            multiUserCommand(command, args)
+        else
+            command(name)
+        end
+    end
+}
+
+cmd.newgame = {
+    'admin',
+    function (name, args)
+        map = args[1] or nil
+        tfm.exec.newGame(map)
+    end
+}
